57cae33e592bd3048b6a446069b3b21178ca57f2
Hunter Loftis <hunter@hunterloftis.com>
2016-02-01 13:26:59 -0500

    use /.tmp

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '.tmp')));
+app.use(express.static(path.join(__dirname, '/.tmp')));
 app.use('/', routes);
 app.use('/users', users);
 
